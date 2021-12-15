d0313e303718abd2c7c75b35565012c4dad646b6
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-12-15 11:27:40 -0300

    feat: creating getUniqueId util function

---
+++ src/utils/getUniqueId.ts
@@ -0,0 +1,7 @@
+const getUniqueId = (prefix = 'id') => {
+  // Convert it to base 36 (numbers + letters), and grab the first 5 characters
+  // after the decimal.
+  return `${prefix}_${Math.random().toString(36).substr(2, 5)}`;
+};
+
+export default getUniqueId;
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+import getUniqueId from './getUniqueId';
+
+export { getUniqueId };
