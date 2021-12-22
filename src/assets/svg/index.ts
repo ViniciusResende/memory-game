e5a0cbd3937abc969285a7d02adc089862d8f17f
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-12-22 11:25:12 -0300

    feat: aded components assets

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,5 +1,8 @@
-import {ReactComponent as ArrowIcon} from './arrow.svg';
+import { ReactComponent as ArrowIcon } from './arrow.svg';
+import { ReactComponent as SignOutIcon } from './sign-out.svg';
+import { ReactComponent as CloseIcon } from './times.svg';
+import { ReactComponent as PokeballIcon } from './pokeball.svg';
+import { ReactComponent as BoneIcon } from './bone.svg';
+import { ReactComponent as ErrorIcon } from './sensor-alert.svg';
 
-export {
-  ArrowIcon,
-};
+export { ArrowIcon, SignOutIcon, CloseIcon, PokeballIcon, BoneIcon, ErrorIcon };
